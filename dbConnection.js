4396907ebc9d194e4fb6021f7576a08c05460016
AbrorGOF <abror95azimov@gmail.com>
2023-08-30 11:44:27 +0500

    Add env vars

--- dbConnection.js
+++ dbConnection.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host: 'localhost', // Replace with your host name
-  user: 'admin',      // Replace with your database username
-  password: 'Od!ilbe1k23',      // Replace with your database password
-  database: 'hello_express' // // Replace with your database Name
+  host: process.env.DB_HOST, // Replace with your host name
+  user: process.env.DB_USERNAME,      // Replace with your database username
+  password: process.env.DB_PASSWORD,      // Replace with your database password
+  database: process.env.DB_DATABASE // // Replace with your database Name
 }); 
  
 conn.connect(function(err) {
